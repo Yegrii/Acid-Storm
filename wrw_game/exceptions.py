023fded00b6dc3560d81c72fc81e13c35418a8cd
Yegor <viryutinyegor@gmail.com>
2023-01-24 22:46:59 +0200

    reformat file

--- wrw_game/exceptions.py
+++ wrw_game/exceptions.py
@@ -4,6 +4,7 @@ class EnemyDown(Exception):
     def __init__(self, enemy):
         self.enemy = enemy
 
+
 class GameOver(Exception):
     #  Ініціалізуємо клас GameOver
     def __init__(self, player):
