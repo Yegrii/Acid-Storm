66f0b227fecc216f9aef50298fccf0d779a43bed
Yegor <viryutinyegor@gmail.com>
2023-01-24 22:46:03 +0200

    change player/enemy class, add name input

--- wrw_game/models.py
+++ wrw_game/models.py
@@ -1,16 +1,19 @@
 from settings import PLAYER_HEALTH, ENEMY_HEALTH
-# Version WRW_game 1.0
+
+
+# Version WRW_game 1.1
 class Player:
     # Ініціалізація гравця
-    def __init__(self, name="Player", health=PLAYER_HEALTH, score=0):
+    # Додано змінну name, щоб гравець міг ввести своє ім‘я
+    def __init__(self, name=input("Enter player's name: "), health=PLAYER_HEALTH, score=0):
         self.name = name
         self.health = health
         self.score = score
 
+
 class Enemy:
     # Ініціалізація ворога
-    def __init__(self, level=1, health=ENEMY_HEALTH, score=0):
-        self.level = level
+    def __init__(self, name=input("Enter enemy's name: "), health=ENEMY_HEALTH, score=0):
+        self.name = name
         self.health = health
         self.score = score
-
